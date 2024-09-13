6bb95bf919f15431059bf155b96bf8058d454e2a
Sebastien Ouler <sebastien.ouler@gmail.com>
2024-09-13 16:15:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# TradingApp
+Clone repo
+git clone https://github.com/ejsgit/TradingApp.git
+
+Create venv, active and install requirement.txt
+python -m venv .venv etc.
+
+do : pip install -r requirements.txt
+LIB installed : notebook, numpy, matplotlib
+
+Test .ipynb : jupyter notebook, open and run test-1.ipynb
+
---
+++ app/exemple.py
@@ -0,0 +1,19 @@
+print("Hello, version 1 with Streamlit")
+
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 2 * np.pi, 100)  # Generate 100 points between 0 and 2π
+y = np.sin(x)# Create a simple plot
+fig, ax = plt.subplots()
+ax.plot(x,y)
+#ax.plot([1, 2, 3], [4, 5, 6])
+#plt.plot(x, y)
+#plt.show()
+
+# Display the plot in Streamlit
+st.pyplot(fig)
+
+# Optionally add some Streamlit interactivity or text
+st.write("This is a simple plot rendered with Streamlit!")
